037c4638830a0cce7b4f627a49d60f566d2a9c61
122-63 <3469@holbertonschool.com>
2022-01-30 20:11:55 -0500

    update

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import String
@@ -8,22 +9,23 @@ import models
 from models.city import City
 
 
-class State(BaseModel, Base):
-    """  Implementation for the State """
-    if models.type_storage == "db":
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    class State(BaseModel, Base):
+        """ State class """
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship('City')
-    else:
-        class State(BaseModel):
-            """ define class"""
-            name = ''
+else:
+    class State(BaseModel):
+        """ Defined class to work with FileStorage'"""
+        name = ''
 
-            @property
-            def cities(self):
-                cities_list = []
-                cities = models.storage.all(city).values()
-                for city in cities:
-                    if city.state_id == self.id:
-                        cities_list.append(city)
-                return cities_list
+        @property
+        def cities(self):
+            """return values"""
+            cities = models.storage.all(City).values()
+            lis_values = []
+            for i in cities:
+                if i.state_id == self.id:
+                    lis_values.append(i)
+            return lis_values
